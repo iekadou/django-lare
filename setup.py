7a506e102b3abd9e46e182a4cf11b6ede5e8525e
Jonas <jbraun@uni-koblenz.de>
2015-08-03 09:59:35 +0200

    fixing spaces in setup.py

--- setup.py
+++ setup.py
@@ -8,55 +8,55 @@ import sys
 
 
 def get_version(package):
-  """
-  Return package version as listed in `__version__` in `init.py`.
-  """
-  init_py = open(os.path.join(package, '__init__.py')).read()
-  return re.match("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.match("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
 
 
 def get_packages(package):
-  """
-  Return root package and all sub-packages.
-  """
-  return [dirpath
-          for dirpath, dirnames, filenames in os.walk(package)
-          if os.path.exists(os.path.join(dirpath, '__init__.py'))]
+    """
+    Return root package and all sub-packages.
+    """
+    return [dirpath
+            for dirpath, dirnames, filenames in os.walk(package)
+            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
 
 
 def get_package_data(package):
-  """
-  Return all files under the root package, that are not in a
-  package themselves.
-  """
-  walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
-          for dirpath, dirnames, filenames in os.walk(package)
-          if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
+    """
+    Return all files under the root package, that are not in a
+    package themselves.
+    """
+    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
+            for dirpath, dirnames, filenames in os.walk(package)
+            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
 
-  filepaths = []
-  for base, filenames in walk:
-      filepaths.extend([os.path.join(base, filename)
-                        for filename in filenames])
-  return {package: filepaths}
+    filepaths = []
+    for base, filenames in walk:
+        filepaths.extend([os.path.join(base, filename)
+                          for filename in filenames])
+    return {package: filepaths}
 
 
 version = get_version('django_lare')
 
 
 if sys.argv[-1] == 'publish':
-  os.system("python setup.py sdist upload")
-  print("You probably want to also tag the version now:")
-  print("  git tag -a %s -m 'version %s'" % (version, version))
-  print("  git push --tags")
-  sys.exit()
+    os.system("python setup.py sdist upload")
+    print("You probably want to also tag the version now:")
+    print("  git tag -a %s -m 'version %s'" % (version, version))
+    print("  git push --tags")
+    sys.exit()
 
 
 setup(name='django-lare',
       version=version,
-	  author='Lare-team',
-	  author_email='lare-team@lare.team',
+      author='Lare Team',
+      author_email='lare-team@lare.team',
       license='MIT',
-	  url='http://lare-team.de',
+      url='http://lare-team.de',
       description='Lare extension to use lare.js easily!',
       packages=get_packages('django_lare'),
       package_data=get_package_data('django_lare'),
@@ -72,10 +72,3 @@ setup(name='django-lare',
           'Topic :: Internet :: WWW/HTTP',
       ]
       )
-      
-
-# (*) Please direct queries to the discussion group, rather than to me directly
-#     Doing so helps ensure your question is helpful to other users.
-#     Queries directly to my email are likely to receive a canned response.
-#
-#     Many thanks for your understanding.
